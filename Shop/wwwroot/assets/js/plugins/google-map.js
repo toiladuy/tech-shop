064e6c51f70f1c69df734f1a56cb030fe4541ed8
Duy Tran <toiladuy10@gmail.com>
2023-02-06 20:45:03 +0700

    Update map

--- Shop/wwwroot/assets/js/plugins/google-map.js
+++ Shop/wwwroot/assets/js/plugins/google-map.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: 10.0378343, lng: 105.7821322 };
+    const uluru = { lat: 10.87766170295706, lng: 106.80162579128859 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("google-map"), {
         zoom: 4,
